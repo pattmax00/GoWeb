3d80b95f55b56988bb4e4328b68510f4a03d5dd9
Maximilian <max@mpatterson.xyz>
2023-07-07 18:05:17 -0500

    Initial wrapper implementation

---
+++ middleware/wrapper.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "net/http"
+
+// ProcessMiddleware is a wrapper function for the http.HandleFunc function
+// that takes the function you want to execute (f) and the middleware you want to execute (m)
+func ProcessMiddleware(f func(w http.ResponseWriter, r *http.Request), m []func()) func(w http.ResponseWriter, r *http.Request) {
+	for _, middleware := range m {
+		middleware()
+	}
+
+	return f
+}
