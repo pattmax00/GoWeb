eb36156c5227be0c676ffd2f46067579d9847a27
Maximilian <max@mpatterson.xyz>
2023-07-21 16:26:43 -0500

    Change function name to ProcessGroup

--- middleware/wrapper.go
+++ middleware/wrapper.go
@@ -2,10 +2,10 @@ package middleware
 
 import "net/http"
 
-// ProcessMiddleware is a wrapper function for the http.HandleFunc function
+// ProcessGroup is a wrapper function for the http.HandleFunc function
 // that takes the function you want to execute (f) and the middleware you want
 // to execute (m) this should be used when processing multiple groups of middleware at a time
-func ProcessMiddleware(f func(w http.ResponseWriter, r *http.Request), m []func()) func(w http.ResponseWriter, r *http.Request) {
+func ProcessGroup(f func(w http.ResponseWriter, r *http.Request), m []func()) func(w http.ResponseWriter, r *http.Request) {
 	for _, middleware := range m {
 		middleware()
 	}
